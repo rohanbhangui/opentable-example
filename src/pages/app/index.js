afe5d005443caf9d84034447710ae2e7353fb6f4
Rohan Bhangui <bhanguirohan@gmail.com>
2019-07-23 21:59:21 -0400

    fix builds

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -4,8 +4,11 @@ import { Provider } from 'react-redux';
 import Main from '../main';
 import TopBar from '../../components/top-bar';
 
+import './index.scss';
+
 const App = ({ store }) => (
   <Provider store={store}>
+    <a className="skip-main" href="#Main">Skip to main content</a>
     <header>
       <TopBar />
     </header>
